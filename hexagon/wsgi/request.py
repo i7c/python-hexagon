308ec48bfa796f8133eb8b14a8e4ae489fc1b630
Constantin Michael Weisser <i7c@posteo.de>
2024-07-31 22:42:56 -0300

    By default try to parse json response

--- hexagon/wsgi/request.py
+++ hexagon/wsgi/request.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from typing import Any, Callable, List, Dict, Optional
 import aws_lambda_wsgi
+import json
 
 
 class WsgiRequestBuilder(object):
@@ -8,23 +9,25 @@ class WsgiRequestBuilder(object):
         # type: () -> None
         self.method: str = 'GET'
         self.path: str = '/'
-        self.protocol: str = 'http'
-        self.proxies: List[str] = ['1.2.3.4']
         self.query_params: Dict[str, str] = {}
+        self.headers: Dict[str, str] = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'x-forwarded-for': "1.2.3.4",
+            'x-forwarded-proto': 'http',
+        }
 
     def render(self):
         # type: () -> dict[str, Any]
         return {
             'httpMethod': self.method,
             'headers': {
-                'x-forwarded-for': ", ".join(self.proxies),
-                'x-forwarded-proto': self.protocol,
             },
             'path': self.path,
             'queryStringParameters': self.query_params
         }
 
-    def make(self, handler=None, app=None):
+    def perform_request_against_app_or_handler(self, handler=None, app=None):
         # type: (Optional[Callable[[dict, Any], Any]], Optional[Flask]) -> Any
         if app:
             return aws_lambda_wsgi.response(app, self.render(), None)
@@ -32,6 +35,15 @@ class WsgiRequestBuilder(object):
             return handler(self.render(), None)
         else:
             raise ValueError("You need to pass either an app or a handler")
+
+    def make(self, handler=None, app=None):
+        # type: (Optional[Callable[[dict, Any], Any]], Optional[Flask]) -> Any
+        response = self.perform_request_against_app_or_handler(handler=handler, app=app)
+        try:
+            response['body'] = json.loads(response['body'])
+        except Exception:
+            pass
+        return response
 
     def with_path(self, p):
         # type: (str) -> WsgiRequestBuilder
