b72a9cbf948c8ca2c2448322929e9d3e6e3d4a94
Constantin Michael Weisser <i7c@posteo.de>
2024-07-31 22:22:58 -0300

    Support more params

--- hexagon/wsgi/request.py
+++ hexagon/wsgi/request.py
@@ -1,11 +1,14 @@
+from typing import Any, Callable, List, Dict
+
+
 class WsgiRequestBuilder(object):
     def __init__(self):
         # type: () -> None
         self.method: str = 'GET'
         self.path: str = '/'
         self.protocol: str = 'http'
-        self.proxies: [str] = ['1.2.3.4']
-        self.query_params: dict[str, str] = {}
+        self.proxies: List[str] = ['1.2.3.4']
+        self.query_params: Dict[str, str] = {}
 
     def render(self):
         # type: () -> dict[str, Any]
@@ -22,6 +25,21 @@ class WsgiRequestBuilder(object):
     def make(self, handler):
         # type: (Callable[[dict, Any], Any]) -> Any
         return handler(self.render(), None)
+
+    def with_path(self, p):
+        # type: (str) -> WsgiRequestBuilder
+        self.path = p
+        return self
+
+    def with_method(self, m):
+        # type (str) -> WsgiRequestBuilder
+        self.method = m
+        return self
+
+    def with_query_param(self, k, v):
+        # type (str, str) -> WsgiRequestBuilder
+        self.query_params[k] = v
+        return self
 
 
 def start() -> WsgiRequestBuilder:
