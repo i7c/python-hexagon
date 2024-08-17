24da06fa6a51be1c5dcfaf62c1f6fca3595ba326
Constantin Michael Weisser <i7c@posteo.de>
2024-08-17 12:23:35 -0300

    Add helper for local debugging

--- hexagon/system/integration.py
+++ hexagon/system/integration.py
@@ -7,3 +7,8 @@ import os
 def aws_lambda_setup(init: Callable[[Flask], Flask], appname: str) -> None:
     if os.getenv("AWS_LAMBDA_FUNCTION_NAME"):
         init(App(appname))
+
+
+def local_debug_setup(init: Callable[[Flask], Flask], appname: str) -> None:
+    if os.getenv("HEXAGON_DEBUG"):
+        init(App(appname)).run(debug=True)
